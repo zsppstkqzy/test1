abd022152d0c419916e5b0c818fd832159fbd75f
zsp <1446166028@qq.com>
2018-02-27 13:12:59 +0800

    点击view响应的方法

--- test1/test1/ViewController.m
+++ test1/test1/ViewController.m
@@ -16,14 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    
 }
-
 
 @end
