b42c8d109d3a1a731e0f41fa54c5d3e5eef94786
zsp <1446166028@qq.com>
2018-02-27 13:10:45 +0800

    初始化项目

---
+++ test1/test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by zsp on 2018/2/27.
+//  Copyright © 2018年 zsp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by zsp on 2018/2/27.
+//  Copyright © 2018年 zsp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
